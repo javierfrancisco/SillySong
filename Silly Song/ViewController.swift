5b92a9b31886a30d61285ff509079da4a0c9b98c
zenkiu <javierfrancisco@gmail.com>
2017-02-06 22:05:28 -0500

    Add feature: Version 1 of Silly Song.

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,67 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func reset(_ sender: Any) {
+        
+        nameField.text = ""
+        lyricsView.text = ""
+        
+        
+    }
 
+
+    @IBAction func displayLyrics(_ sender: Any) {
+    
+        if( nameField.text != ""){
+        
+            let lyrics = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: nameField.text!)
+            
+            lyricsView.text = lyrics
+        }
+      
+        
+    }
 }
+
+
+func shortNameFromName(name: String) -> String {
+    // implementation here
+    
+    let lowercaseName = name.lowercased()
+    let vowelSet = CharacterSet(charactersIn: "aeiou")
+    
+    let indexOfFirstVowel = lowercaseName.rangeOfCharacter(from: vowelSet)
+    
+    let res = lowercaseName.substring(from: (indexOfFirstVowel?.lowerBound)!)
+    
+    return res
+}
+
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
+
+// join an array of strings into a single template string:
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"].joined(separator: "\n")
+
+func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+    
+    let shortName = shortNameFromName(name: fullName)
+    
+    var templateTemp  = lyricsTemplate.replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+    templateTemp  = templateTemp.replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+    
+    return templateTemp
+}
+
+
 
